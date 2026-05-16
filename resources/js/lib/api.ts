2a1d1c9eadec8ade40ecc216c2e6afc74d6c84f6
Po Ming Lam <poming.lam@gmail.com>
2026-05-15 22:43:21 -0400

    Update API timeout setting to 60 seconds and maintain existing headers configuration

--- resources/js/lib/api.ts
+++ resources/js/lib/api.ts
@@ -1,10 +1,8 @@
 import axios, { type AxiosInstance } from 'axios';
 
 export const api: AxiosInstance = axios.create({
-  timeout: 15000,
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-  },
+    timeout: 60000,
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+    },
 });
-
-
